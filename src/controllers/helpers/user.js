27de4e052e945ce52ac341a99d0e3aa46ed0b511
PedroRomaoDev <pedro7ntj@gmail.com>
2025-02-23 00:34:02 -0300

    feat: add helper functions to user controllers

---
+++ src/controllers/helpers/user.js
@@ -0,0 +1,21 @@
+import { badRequest } from './http.js';
+import validator from 'validator';
+
+export const invalidPasswordResponse = () =>
+    badRequest({
+        message: 'Password must be at least 6 characters',
+    });
+
+export const emailIsAlreadyInUseResponse = () =>
+    badRequest({
+        message: 'Invalid email. Please provide a valid one',
+    });
+
+export const invalidIdResponse = () =>
+    badRequest({
+        message: 'Some provided field is not allowed.',
+    });
+
+export const checkIfPasswordIsValid = (password) => password.length >= 6;
+
+export const checkIfEmailIsValid = (email) => validator.isEmail(email);
--- src/controllers/update-user.js
+++ src/controllers/update-user.js
@@ -1,7 +1,14 @@
 import validator from 'validator';
-import { badRequest, ok, serverError } from './helpers.js';
+import { badRequest, ok, serverError } from './helpers/http.js';
 import { UpdateUserUseCase } from '../use-cases/update-user.js';
 import { EmailAlreadyInUseError } from '../errors/user.js';
+import {
+    checkIfEmailIsValid,
+    checkIfPasswordIsValid,
+    emailIsAlreadyInUseResponse,
+    invalidIdResponse,
+    invalidPasswordResponse,
+} from './helpers/user.js';
 
 export class UpdateUserController {
     async execute(httpRequest) {
@@ -16,7 +23,7 @@ export class UpdateUserController {
                 });
             }
 
-            const updateUserParams = httpRequest.body;
+            const params = httpRequest.body;
 
             const allowedFields = [
                 'first_name',
@@ -25,42 +32,33 @@ export class UpdateUserController {
                 'password',
             ];
 
-            const someFieldIsNotAllowed = Object.keys(updateUserParams).some(
+            const someFieldIsNotAllowed = Object.keys(params).some(
                 (field) => !allowedFields.includes(field),
             );
 
             if (someFieldIsNotAllowed) {
-                return badRequest({
-                    message: 'Some provided field is not allowed.',
-                });
+                return invalidIdResponse();
             }
 
-            if (updateUserParams.password) {
-                const passwordIsNotValid = updateUserParams.password.length < 6;
+            if (params.password) {
+                const passwordIsValid = checkIfPasswordIsValid(params.password);
 
-                if (passwordIsNotValid) {
-                    return badRequest({
-                        message: 'Password must be at least 6 characters',
-                    });
+                if (passwordIsValid) {
+                    return invalidPasswordResponse();
                 }
             }
 
-            if (updateUserParams.email) {
-                const emailIsValid = validator.isEmail(updateUserParams.email);
+            if (params.email) {
+                const emailIsValid = checkIfEmailIsValid();
 
                 if (!emailIsValid) {
-                    return badRequest({
-                        message: 'Invalid e-mail. Please provide a valid one.',
-                    });
+                    return emailIsAlreadyInUseResponse(params.email);
                 }
             }
 
             const updateUserUseCase = new UpdateUserUseCase();
 
-            const updatedUser = await updateUserUseCase.execute(
-                userId,
-                updateUserParams,
-            );
+            const updatedUser = await updateUserUseCase.execute(userId, params);
 
             return ok(updatedUser);
         } catch (error) {
