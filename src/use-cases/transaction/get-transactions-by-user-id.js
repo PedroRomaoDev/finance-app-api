01a94b0f3d2428930fc0ff54140b7c5bf22f0e44
PedroRomaoDev <pedro7ntj@gmail.com>
2025-03-15 16:46:28 -0300

    feat: add GetTransactionsByUserIdUseCase

---
+++ src/use-cases/transaction/get-transactions-by-user-id.js
@@ -0,0 +1,23 @@
+import { userNotFoundResponse } from '../../controllers/index.js';
+
+export class GetTransactionsByUserId {
+    constructor(getTransactionsByUserIdRepository, getUserByIdRepository) {
+        this.getTransactionsByUserIdRepository =
+            getTransactionsByUserIdRepository;
+        this.getUserByIdRepository = getUserByIdRepository;
+    }
+    async execute(params) {
+        //validar se o usuario existe
+        const user = await this.getUserByIdRepository.execute(params.userId);
+
+        if (!user) {
+            return userNotFoundResponse();
+        }
+
+        //se existe, chamar o repository
+        const transactions =
+            await this.getTransactionsByUserIdRepository.execute(params.userId);
+
+        return transactions;
+    }
+}
