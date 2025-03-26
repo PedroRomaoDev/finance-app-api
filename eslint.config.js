0f78f2eec15a89dc50f9f6c6154aabd20fa5132d
PedroRomaoDev <pedro7ntj@gmail.com>
2025-03-26 18:27:43 -0300

    chore: add initial jest setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,14 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.jest, // Adiciona as globais do Jest
+            },
+        },
+    },
+    pluginJs.configs.recommended,
 ];
