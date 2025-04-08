ed846859cf566603476fdba3275bfcea574d94d5
PedroRomaoDev <pedro7ntj@gmail.com>
2025-04-08 17:35:55 -0300

    test: ensure CreateUserUseCase throw an EmailAlreadyInUseError if GetUserByEmalRepository returns a user

--- src/use-cases/user/create-user.test.js
+++ src/use-cases/user/create-user.test.js
@@ -1,5 +1,6 @@
 import { faker } from '@faker-js/faker';
 import { CreateUserUseCase } from './create-user';
+import { EmailAlreadyInUseError } from '../../errors/user';
 
 describe('CreateUserUseCase', () => {
     class GetUserByEmailRepositoryStub {
@@ -25,6 +26,15 @@ describe('CreateUserUseCase', () => {
             return 'generated_id';
         }
     }
+
+    const user = {
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({
+            length: 7,
+        }),
+    };
 
     const makeSut = () => {
         const getUserByEmailRepository = new GetUserByEmailRepositoryStub();
@@ -64,5 +74,21 @@ describe('CreateUserUseCase', () => {
 
         // assert
         expect(createdUser).toBeTruthy();
+    });
+
+    it('should throw an EmailAlrealdyInUseError if GetUserByEmailRepository returns a user', async () => {
+        // arrange
+        const { sut, getUserByEmailRepository } = makeSut();
+        jest.spyOn(getUserByEmailRepository, 'execute').mockReturnValueOnce(
+            user,
+        );
+
+        // act
+        const promise = sut.execute(user);
+
+        // assert
+        await expect(promise).rejects.toThrow(
+            new EmailAlreadyInUseError(user.email),
+        );
     });
 });
