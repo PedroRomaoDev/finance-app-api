f44d4be41ccfc2dcc1e94b7ebfbfc9886f2c53e8
PedroRomaoDev <pedro7ntj@gmail.com>
2025-04-12 16:54:07 -0300

    test: ensure CreateTransactionUseCase throws UserNotFoundError if user does exist

--- src/use-cases/transaction/create-transaction.test.js
+++ src/use-cases/transaction/create-transaction.test.js
@@ -1,5 +1,6 @@
 import { faker } from '@faker-js/faker';
 import { CreateTransactionUseCase } from './create-transaction.js';
+import { UserNotFoundError } from '../../errors/user';
 
 describe('CreateTransactionUseCase', () => {
     class CreateTransactionRepositoryStub {
@@ -111,5 +112,19 @@ describe('CreateTransactionUseCase', () => {
             ...createTransactionParams,
             id: 'random_id',
         });
+    });
+
+    it('should throw UserNotFoundError if user does not exist', async () => {
+        // arrange
+        const { sut, getUserByIdRepository } = makeSut();
+        jest.spyOn(getUserByIdRepository, 'execute').mockResolvedValue(null);
+
+        // act
+        const promise = sut.execute(createTransactionParams);
+
+        // assert
+        await expect(promise).rejects.toThrow(
+            new UserNotFoundError(createTransactionParams.user_id),
+        );
     });
 });
