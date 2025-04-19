dce7a6e3571b21ead37efe5b90f3f3b84458cc35
PedroRomaoDev <pedro7ntj@gmail.com>
2025-04-19 11:30:48 -0300

    test:  PosgtresGetUserBalanceRepository calls Prisma with correct params

--- src/repositories/postgres/user/get-user-balance.test.js
+++ src/repositories/postgres/user/get-user-balance.test.js
@@ -63,4 +63,40 @@ describe('PostgresGetUserBalanceRepository', () => {
         expect(result.investments.toString()).toBe('6000');
         expect(result.balance.toString()).toBe('2000');
     });
+    it('should calls Prisma with correct params ', async () => {
+        const sut = new PostgresGetUserBalanceRepository();
+        const prismaSpy = jest.spyOn(prisma.transaction, 'aggregate');
+        const userId = faker.string.uuid();
+
+        await sut.execute(userId);
+
+        expect(prismaSpy).toHaveBeenCalledTimes(3);
+        expect(prismaSpy).toHaveBeenCalledWith({
+            where: {
+                user_id: userId,
+                type: 'EARNING',
+            },
+            _sum: {
+                amount: true,
+            },
+        });
+        expect(prismaSpy).toHaveBeenCalledWith({
+            where: {
+                user_id: userId,
+                type: 'EXPENSE',
+            },
+            _sum: {
+                amount: true,
+            },
+        });
+        expect(prismaSpy).toHaveBeenCalledWith({
+            where: {
+                user_id: userId,
+                type: 'INVESTMENT',
+            },
+            _sum: {
+                amount: true,
+            },
+        });
+    });
 });
