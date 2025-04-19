48116046e09e4280bd3604025afdf8ea6a82d614
PedroRomaoDev <pedro7ntj@gmail.com>
2025-04-19 11:38:19 -0300

    test:  PostgresGetUserByEmailRepository get a user by email on db

---
+++ src/repositories/postgres/user/get-user-by-email.test.js
@@ -0,0 +1,16 @@
+import { prisma } from '../../../../prisma/prisma.js';
+import { PostgresGetUserByEmailRepository } from './get-user-by-email.js';
+import { user as fakeUser } from '../../../tests';
+
+describe('GetUserByEmailRepository', () => {
+    it('should get user by email on db', async () => {
+        const user = await prisma.user.create({
+            data: fakeUser,
+        });
+        const sut = new PostgresGetUserByEmailRepository();
+
+        const result = await sut.execute(user.email);
+
+        expect(result).toStrictEqual(user);
+    });
+});
