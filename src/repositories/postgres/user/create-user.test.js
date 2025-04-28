dec14e4756a5b135c7ebb26d0dfaca9e370693fa
PedroRomaoDev <pedro7ntj@gmail.com>
2025-04-27 22:55:53 -0300

    test: ensure PostgresCreateUserRepository throw if Prisma throws

--- src/repositories/postgres/user/create-user.test.js
+++ src/repositories/postgres/user/create-user.test.js
@@ -31,4 +31,13 @@ describe('PostgresCreateUserRepository', () => {
             data: user,
         });
     });
+
+    it('should throw if Prisma throws', async () => {
+        const sut = new PostgresCreateUserRepository();
+        jest.spyOn(prisma.user, 'create').mockRejectedValueOnce(new Error());
+
+        const promise = sut.execute(user);
+
+        await expect(promise).rejects.toThrow();
+    });
 });
