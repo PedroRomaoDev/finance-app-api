8e2199728d4ba1da6f3baa8bec4f8f211e432975
PedroRomaoDev <pedro7ntj@gmail.com>
2025-05-01 11:43:55 -0300

    refactor: move transactions route to separate file

--- index.js
+++ index.js
@@ -1,51 +1,12 @@
 import 'dotenv/config.js';
 import express from 'express';
-import {
-    makeGetTransactionsByUserIdController,
-    makeCreateTransactionController,
-    makeUpdateTransactionController,
-    makeDeleteTransactionController,
-} from './src/factories/controllers/transaction.js';
-import { usersRouter } from './src/routes/users.js';
+import { usersRouter, transactionsRouter } from './src/routes/index.js';
 
 const app = express();
 
 app.use(express.json());
 app.use('/api/users', usersRouter);
-
-app.get('/api/transactions', async (request, response) => {
-    const getTransactionsByUserIdController =
-        makeGetTransactionsByUserIdController();
-
-    const { statusCode, body } =
-        await getTransactionsByUserIdController.execute(request);
-    response.status(statusCode).send(body);
-});
-
-app.post('/api/transactions', async (request, response) => {
-    const createTransactionController = makeCreateTransactionController();
-
-    const { statusCode, body } =
-        await createTransactionController.execute(request);
-    response.status(statusCode).send(body);
-});
-
-app.patch('/api/transactions/:transactionId', async (request, response) => {
-    const updateTransationController = makeUpdateTransactionController();
-    const { statusCode, body } =
-        await updateTransationController.execute(request);
-
-    response.status(statusCode).send(body);
-});
-
-app.delete('/api/transactions/:transactionId', async (request, response) => {
-    const deleteTransactionController = makeDeleteTransactionController();
-
-    const { statusCode, body } =
-        await deleteTransactionController.execute(request);
-
-    response.status(statusCode).send(body);
-});
+app.use('/api/transactions', transactionsRouter);
 
 // eslint-disable-next-line no-undef
 app.listen(process.env.PORT, () =>
