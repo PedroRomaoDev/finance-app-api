6a6abd97b9ea78f4329565907e2a138a100c61fc
PedroRomaoDev <pedro7ntj@gmail.com>
2025-05-01 18:12:45 -0300

    test: ensure GET /api/users/:userId returns 200 when user is found

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 import 'dotenv/config.js';
-import express from 'express';
-import { usersRouter, transactionsRouter } from './src/routes/index.js';
-
-export const app = express();
-
-app.use(express.json());
-app.use('/api/users', usersRouter);
-app.use('/api/transactions', transactionsRouter);
+import { app } from './src/app.js';
 
 // eslint-disable-next-line no-undef
 app.listen(process.env.PORT, () =>
