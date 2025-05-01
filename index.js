256da1c4ca30924dbfbe2de46e42332caf564561
PedroRomaoDev <pedro7ntj@gmail.com>
2025-05-01 11:34:03 -0300

    refactor: move user route to separate file

--- index.js
+++ index.js
@@ -1,54 +1,17 @@
 import 'dotenv/config.js';
 import express from 'express';
 import {
-    makeGetUserByIdController,
-    makeCreateUserController,
-    makeUpdateUserController,
-    makeDeleteUserController,
-    makeGetUserBalanceController,
-} from './src/factories/controllers/user.js';
-import {
     makeGetTransactionsByUserIdController,
     makeCreateTransactionController,
     makeUpdateTransactionController,
     makeDeleteTransactionController,
 } from './src/factories/controllers/transaction.js';
+import { usersRouter } from './src/routes/users.js';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/api/users/:userId', async (request, response) => {
-    const getUserByIdController = makeGetUserByIdController();
-
-    const { statusCode, body } = await getUserByIdController.execute(request);
-
-    response.status(statusCode).send(body);
-});
-
-app.post('/api/users', async (request, response) => {
-    const createUserController = makeCreateUserController();
-
-    const { statusCode, body } = await createUserController.execute(request);
-
-    response.status(statusCode).send(body);
-});
-
-app.patch('/api/users/:userId', async (request, response) => {
-    const updateUserController = makeUpdateUserController();
-
-    const { statusCode, body } = await updateUserController.execute(request);
-
-    response.status(statusCode).send(body);
-});
-
-app.delete('/api/users/:userId', async (request, response) => {
-    const deleteUserController = makeDeleteUserController();
-
-    const { statusCode, body } = await deleteUserController.execute(request);
-
-    response.status(statusCode).send(body);
-});
+app.use('/api/users', usersRouter);
 
 app.get('/api/transactions', async (request, response) => {
     const getTransactionsByUserIdController =
@@ -72,13 +35,6 @@ app.patch('/api/transactions/:transactionId', async (request, response) => {
     const { statusCode, body } =
         await updateTransationController.execute(request);
 
-    response.status(statusCode).send(body);
-});
-
-app.get('/api/users/:userId/balance', async (request, response) => {
-    const getUserBalanceController = makeGetUserBalanceController();
-    const { statusCode, body } =
-        await getUserBalanceController.execute(request);
     response.status(statusCode).send(body);
 });
 
