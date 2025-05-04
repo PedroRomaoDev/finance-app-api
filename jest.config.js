5072492bdf04e623137a158d5150d8fe7f665323
PedroRomaoDev <pedro7ntj@gmail.com>
2025-05-04 13:26:49 -0300

    feat: corrected all tests e2e

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,14 @@
-/**
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
+// jest.config.mjs
 
-/** @type {import('jest').Config} */
-const config = {
+export default {
+    transform: {
+        '^.+\\.js$': 'babel-jest', // Transforma arquivos .js com babel-jest
+    },
+    testEnvironment: 'node',
+    moduleFileExtensions: ['js', 'json', 'node'],
     coverageDirectory: 'coverage',
     coverageProvider: 'v8',
     collectCoverageFrom: ['src/**/*.js'],
     globalSetup: '<rootDir>/jest.global-setup.js',
     setupFilesAfterEnv: ['<rootDir>/jest.setup-after-env.js'],
 };
-
-export default config;
--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ import swaggerUi from 'swagger-ui-express';
 import fs from 'fs';
 import path from 'path';
 
+//ambiente ES Modules - path para determinar o __dirname
+const __dirname = path.resolve(); //vai funcionar sem importar diretamente `import.meta.url`
+
 export const app = express();
 
 app.use(express.json());
@@ -12,10 +15,7 @@ app.use('/api/users', usersRouter);
 app.use('/api/transactions', transactionsRouter);
 
 const swaggerDocument = JSON.parse(
-    fs.readFileSync(
-        path.join(import.meta.dirname, '../docs/swagger.json'),
-        'utf8',
-    ),
+    fs.readFileSync(path.join(__dirname, './docs/swagger.json'), 'utf8'),
 );
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
