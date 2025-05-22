10b93b59dc5d628815bde052b94cf1ac8157482c
PedroRomaoDev <pedro7ntj@gmail.com>
2025-05-22 12:31:45 -0300

    feat: add loginUserSchema with zod

--- src/schemas/user.js
+++ src/schemas/user.js
@@ -39,3 +39,23 @@ export const createUserSchema = z.object({
 export const updateUserSchema = createUserSchema.partial().strict({
     message: 'Some provided field is not allowed.',
 });
+
+export const loginUserSchema = z.object({
+    email: z
+        .string({
+            required_error: 'E-mail is required.',
+        })
+        .email({
+            message: 'Please provide a valid email.',
+        })
+        .trim()
+        .min(1),
+    password: z
+        .string({
+            required_error: 'Password is required.',
+        })
+        .trim({})
+        .min(6, {
+            message: 'Password must have at least 6 characters.',
+        }),
+});
