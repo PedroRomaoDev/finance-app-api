4008c96d88d209e1f8aa47609595913ccb9bec28
PedroRomaoDev <pedro7ntj@gmail.com>
2025-05-29 19:40:19 -0300

    test: ensure from and to params are passed correctly to use case

--- src/controllers/transaction/get-transactions-by-user-id.test.js
+++ src/controllers/transaction/get-transactions-by-user-id.test.js
@@ -115,6 +115,6 @@ describe('GetTransactionsByUserIdController', () => {
         });
 
         // assert
-        expect(executeSpy).toHaveBeenCalledWith(userId);
+        expect(executeSpy).toHaveBeenCalledWith(userId, from, to);
     });
 });
