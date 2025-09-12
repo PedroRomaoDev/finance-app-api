b657f243aad9725bedb47a2c06fd74690338302e
PedroRomaoDev <pedro7ntj@gmail.com>
2025-09-11 23:15:33 -0300

    feat: add cors

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ import { usersRouter, transactionsRouter } from './routes/index.js';
 import swaggerUi from 'swagger-ui-express';
 import fs from 'fs';
 import path from 'path';
+import cors from 'cors';
 
 //ambiente ES Modules - path para determinar o __dirname
 const __dirname = path.resolve(); //vai funcionar sem importar diretamente `import.meta.url`
 
 export const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', usersRouter);
